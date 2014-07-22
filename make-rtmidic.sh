868ad9619486236a01a011d2e54993b15cb2bd6b
Ben Swift <benjamin.j.swift@gmail.com>
2014-07-23 09:19:37 +1000

    add Linux build command to make-rtmidic.sh

--- make-rtmidic.sh
+++ make-rtmidic.sh
@@ -3,11 +3,13 @@
 case $(uname) in
     # *NT*)
 
-	  # Linux)
-
+	  Linux)
+        g++ -shared -fPIC -Wall -D__LINUX_ALSA__ RtMidi.cpp RtMidiC.cpp -lasound -lpthread -o librtmidic.so ;;
     
 	  Darwin)
         clang++ -dynamiclib -Wall -D__MACOSX_CORE__ -o librtmidic.dylib RtMidi.cpp RtMidiC.cpp -framework CoreMIDI -framework CoreAudio -framework CoreFoundation ;;
 	  *)
         echo Unsupported OS:  $(uname) >&2 ; exit 1 ;;
 esac
+
+exit 0
